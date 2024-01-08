24f1a90ba773915f5efd491c0bbccc8d6543dacd
Max Bigras <max@standard.ai>
2024-01-08 11:25:24 -0800

    Only check var.firehose_config != null once

--- main.tf
+++ main.tf
@@ -6,19 +6,16 @@ resource "google_pubsub_subscription" "firehose" {
   count = var.firehose_config != null ? 1 : 0
   name  = "hedwig-${var.topic}-firehose"
   topic = "hedwig-${var.topic}"
-  dynamic "cloud_storage_config" {
-    for_each = var.firehose_config != null ? [1] : []
-    content {
-      bucket          = var.firehose_config.bucket
-      filename_prefix = var.firehose_config.filename_prefix
-      filename_suffix = var.firehose_config.filename_suffix
-      max_bytes       = var.firehose_config.max_bytes
-      max_duration    = var.firehose_config.max_duration
-      dynamic "avro_config" {
-        for_each = var.firehose_config.write_avro_format ? [1] : []
-        content {
-          write_metadata = true
-        }
+  cloud_storage_config {
+    bucket          = var.firehose_config.bucket
+    filename_prefix = var.firehose_config.filename_prefix
+    filename_suffix = var.firehose_config.filename_suffix
+    max_bytes       = var.firehose_config.max_bytes
+    max_duration    = var.firehose_config.max_duration
+    dynamic "avro_config" {
+      for_each = var.firehose_config.write_avro_format ? [1] : []
+      content {
+        write_metadata = true
       }
     }
   }
